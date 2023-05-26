620d7067398cef4ff650ec7b433aa906fabbffb5
mridulgulati18 <mridulgulati18@gmail.com>
2023-05-26 17:21:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import ImageCard from './components/imageCard';
+import { useState, useEffect} from 'react';
+import ImageSearch from './components/imagesearch';
 
 function App() {
+  const [images,setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(()=>{
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}s&image_type=photo&pretty=true`)
+    .then(res => res.json())
+    .then( data => {
+      setImages(data.hits);
+      setIsLoading(false);
+    })
+    .catch(err => console.log(err))
+  },[term]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto'>
+      <div className='bgImg text-5xl font-mono text-white text-center py-40'>
+        <h1>VISUAL ARCHIVE</h1>
+      </div>
+      <ImageSearch searchText={(text)=>setTerm(text)}/>
+      {(!isLoading && images.length===0 && <div grid grid-cols-3 gap-4><h1 className="text-6xl text-center mx-auto mt-32 text-pink-300 my-auto">No images found</h1></div>)}
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32 text-pink-300">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image =>(
+          <ImageCard key={image.id} image={image}/>
+        ))}
+      </div>}
     </div>
+    
   );
 }
 
---
+++ src/components/imagesearch.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+
+const ImageSearch = ({searchText}) => {
+    const [text,setText] = useState('');
+    const onSubmit =(e)=>{
+        e.preventDefault();
+        searchText(text);
+    }
+  return (
+    <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
+    <form onSubmit={onSubmit}>   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        </div>
+        <input onChange={e=>setText(e.target.value)} type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-purple-500 focus:border-purple-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500" placeholder="Search Image term" required></input>
+        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Search</button>
+    </div>
+    </form>
+    </div>
+  )
+}
+
+export default ImageSearch
